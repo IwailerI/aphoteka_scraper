d0210b02fc7ea74b2e1b52a02eefa1cc918b01f1
Andrey Pirozhenok <andrypirk@gmail.com>
2024-08-13 15:09:58 +0300

    Added user agent string

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -14,6 +14,7 @@ func FetchData(input map[string]string) (manifest.Manifest, error) {
 
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.apotheka.lv"),
+		colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0"),
 	)
 
 	const XPATH = `//script[@type="application/ld+json"]`
